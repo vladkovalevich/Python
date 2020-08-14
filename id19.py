25c97b5a8112ea9f38c69072d5f4cd562575b9a6
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-08-14 19:23:03 +0300

    ...

--- id19.py
+++
@@ -1,8 +0,0 @@
-rtry = input()
-result = 0
-for i in range(int(len(rtry) /2)):
-        if(rtry[i] == rtry[-i-1]):
-                result +=1
-if(int(len(rtry) % 2 ) != 0):
-    result +=1
-print(result)
