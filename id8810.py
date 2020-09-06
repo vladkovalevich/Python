c49201d63177f05ebf03e259a6aec73f9a9eea22
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-09-06 10:14:45 +0300

    ......

--- id8810.py
+++
@@ -1,2 +0,0 @@
-n = input().split(" ")
-print(int(n[0])+(int(n[1])- int(n[2])))
--- id8812.py
+++
@@ -1,4 +0,0 @@
-n = input().split(" ")
-P = 2*(int(n[0])+int(n[1]))
-S = int(n[0])*int(n[1])
-print(str(P) +" "+ str(S))
